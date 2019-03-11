c3f5650d754f4e4de735980b4644b17f79e125f7
Darryl <darrylmangibin@gmail.com>
2019-03-11 08:56:24 +0800

    redux devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,7 +8,8 @@ export default () => {
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducers
-    })
-  )
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   return store
 }
