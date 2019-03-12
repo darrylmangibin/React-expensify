6f77ce6144bc7203a861320a9b7a387e2738e591
Darryl <darrylmangibin@gmail.com>
2019-03-12 16:49:56 +0800

    setup database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 
 const config = {
-  apiKey: "AIzaSyB-KaP7Nw6Nx-UdOZ3BnKrjxJNimCb1PE4",
-  authDomain: "expensify-cab9d.firebaseapp.com",
-  databaseURL: "https://expensify-cab9d.firebaseio.com",
-  projectId: "expensify-cab9d",
-  storageBucket: "expensify-cab9d.appspot.com",
-  messagingSenderId: "258750244826"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
