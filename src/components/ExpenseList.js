6e43e63a740bc082d3f80a425083d79249794d42
Darryl <darrylmangibin@gmail.com>
2019-03-12 15:24:08 +0800

    styling expenses list

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,17 +5,35 @@ import getVisibleExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => {
   return (
-    <div>
-      <h1>ExpenseList</h1>
-      {props.expenses.map((expense) => {
-        return <ExpenseListItem 
-          key={expense.id}
-          id={expense.id} 
-          description={expense.description}
-          amount={expense.amount}
-          createdAt={expense.createdAt}
-        />
-      })}
+    <div className="content-container">
+      <div className="list-header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">
+          Expense
+        </div>
+        <div className="show-for-desktop">
+          Amount
+        </div>
+      </div>
+      <div className="list-body">
+        {
+          props.expenses.length === 0 ? (
+            <div className="list-item--message">
+              <span>No expenses</span>
+            </div>
+          ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem 
+                key={expense.id}
+                id={expense.id} 
+                description={expense.description}
+                amount={expense.amount}
+                createdAt={expense.createdAt}
+              />
+            })
+          )
+        }
+      </div>
     </div>
   )
 }
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
 
 const ExpenseListItem = (props) => {
   return (
-    <div>
-      <Link to={`edit/${props.id}`}>
-        <h3>{props.description}</h3>
-      </Link>
-      <p>{props.amount} - {props.createdAt}</p>
-    </div>
+    <Link className="list-item" to={`edit/${props.id}`}>
+      <div>
+        <h3 className="list-item__title">{props.description}</h3>
+        <span className="list-item__subtitle">{moment(props.createdAt).format('MMMM Do, YYYY')}</span>
+      </div>
+      <h3 className="list-item__data">{numeral(props.amount / 100).format('$0.00')}</h3>
+    </Link>
   )
 }
 
