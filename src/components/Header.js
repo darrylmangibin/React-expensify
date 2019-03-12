eeed2d183798c8bcdc414d356c6480a62ac0db34
Darryl <darrylmangibin@gmail.com>
2019-03-12 11:18:57 +0800

    Public Route

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,8 @@ const Header = ({ startLogout }) => {
   return(
     <header>
       <h1>Expensify</h1>
-      <NavLink to="/dashboard" activeClassName="isActive" exact={true}>Dashboard</NavLink>
+      <NavLink to="/dashboard" activeClassName="isActive">Dashboard</NavLink>
       <NavLink to="/create" activeClassName="isActive">Create Expense</NavLink>
-      <NavLink to="/help" activeClassName="isActive">Help</NavLink>
       <button onClick={startLogout}>Logout</button>
     </header>
   )
---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import Header from '../components/Header';
+
+export const PublicRoute = ({ 
+  isAuthenticated,  
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route {...rest} component={(props) => {
+      return isAuthenticated ? (
+        <div>
+          <Header />
+          <Redirect to="/dashboard" />      
+        </div>
+      ) : (
+        <Component {...props} />
+      )
+    }} />
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: !!state.auth.uid
+  }
+}
+
+export default connect(mapStateToProps)(PublicRoute);
